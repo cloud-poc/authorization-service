9d037d3d6def97caae484726c0a9c03c231d20f9
Jamie Zhang <akjamie.zhang@outlook.com>
2019-06-17 18:08:48 +0800

    update

--- src/main/java/org/akj/springboot/authorization/configuration/WebMvcConfiguration.java
+++ src/main/java/org/akj/springboot/authorization/configuration/WebMvcConfiguration.java
@@ -1,10 +1,14 @@
 package org.akj.springboot.authorization.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
 	@Override
