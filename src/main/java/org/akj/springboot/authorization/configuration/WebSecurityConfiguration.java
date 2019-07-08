47e06bd736a8c8fcba4addc6d2d5551f135c5885
Jamie Zhang <akjamie.zhang@outlook.com>
2019-07-09 00:32:59 +0800

    update prometheus config

--- src/main/java/org/akj/springboot/authorization/configuration/WebSecurityConfiguration.java
+++ src/main/java/org/akj/springboot/authorization/configuration/WebSecurityConfiguration.java
@@ -43,7 +43,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/oauth/**", "/login").permitAll().anyRequest()
+		http.csrf().disable().authorizeRequests().antMatchers("/oauth/**", "/login", "/actuator/**").permitAll().anyRequest()
 				.authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
 	}
 
